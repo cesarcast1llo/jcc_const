25e7e1c52d0c0a6cc334070b1e2320a44be1b9e9
cesarcast1llo <cesarcast1llo@yahoo.com>
2019-12-04 16:51:00 -0800

    package.json fix

--- next.config.js
+++ next.config.js
@@ -2,4 +2,11 @@ const withSass = require('@zeit/next-sass');
 
 const config = withSass();
 
-module.exports = config;
+const sitemap = require('nextjs-sitemap-generator');  
+sitemap({  
+  baseUrl: '<your_website_base_url>',  
+  pagesDirectory: __dirname + "/pages",  
+  targetDirectory : 'static/'  
+});
+
+module.exports = config, sitemap;
