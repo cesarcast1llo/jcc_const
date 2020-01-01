8de6ec33e5934cbf32cb59a329e32660b48b901b
cesarcast1llo <cesarcast1llo@yahoo.com>
2019-12-31 18:15:37 -0800

    fixed nav issue done, need styling to create white container after scroll

--- components/Header.js
+++ components/Header.js
@@ -34,14 +34,14 @@ const Header = ({ router }) => {
       scrollPos > 730 ? classAction = 'fadeIn' : classAction = 'fadeOut'
   : null;
 
-  router.pathname === '/portfolio' ?
-    scrollPos > 50 ? classAction = 'fadeOut' : color = 'black' : null;
+  // router.pathname === '/portfolio' ?
+  //   scrollPos > 50 ? classAction = 'fadeOut' : color = 'black' : null;
     // scrollPos > 51 ? classAction = 'fadeIn' : null;
 
 
   return (
-    <div className="header" style={{backgroundColor: `${color}`,}}>
-      <Navbar className={`nav-bar ${classAction}`}>
+    // <div className="header" style={{backgroundColor: `${color}`,}}>
+      <Navbar className={`nav-bar fixed-top ${classAction}`}>
         <NavbarBrand className="image-wrapper">
           <img className="logo-img" src="../static/img/jcc.png" alt="JCC" />
         </NavbarBrand>
@@ -49,7 +49,7 @@ const Header = ({ router }) => {
           <HamBurg />
         </div>
       </Navbar>
-    </div>
+    // </div>
   );
 }
 
